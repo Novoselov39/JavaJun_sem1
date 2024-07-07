ed3480c290b90d0414d8fd73a2c6bda4a1b920ec
Anatolii <novoselov39@gmail.com>
2024-07-07 22:47:31 +0500

    sem1

--- src/main/java/ru/gb/lesson1/hw/Homework.java
+++ src/main/java/ru/gb/lesson1/hw/Homework.java
@@ -1,31 +1,142 @@
 package ru.gb.lesson1.hw;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Homework {
 
   private static class Department {
     private String name;
 
-    // TODO: геттеры, сеттеры
+    public Department(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public String toString() {
+      return "Department{" +
+              "name='" + name + '\'' +
+              '}';
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+// TODO: геттеры, сеттеры
   }
 
   private static class Person {
     private String name;
+
+    @Override
+    public String toString() {
+      return "Person{" +
+              "name='" + name + '\'' +
+              ", age=" + age +
+              ", salary=" + salary +
+              ", depart=" + depart +
+              '}';
+    }
+
     private int age;
     private double salary;
     private Department depart;
 
+    public Person(String name, int age, double salary, Department depart) {
+      this.name = name;
+      this.age = age;
+      this.salary = salary;
+      this.depart = depart;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+
+    public double getSalary() {
+      return salary;
+    }
+
+    public void setSalary(double salary) {
+      this.salary = salary;
+    }
+
+    public Department getDepart() {
+      return depart;
+    }
+
+    public void setDepart(Department depart) {
+      this.depart = depart;
+    }
     // TODO: геттеры, сеттеры
   }
+
+  public static void main(String[] args) {
+
+    List<Person> listPerson =generatePerson(3);
+    System.out.println(listPerson);
+//    System.out.println(findMostYoungestPerson(listPerson));
+//    System.out.println(findMostExpensiveDepartment(listPerson));
+//    System.out.println(groupByDepartment(listPerson));
+//    System.out.println(groupByDepartmentName(listPerson));
+//    System.out.println(getDepartmentOldestPerson(listPerson));
+    System.out.println(cheapPersonsInDepartment(listPerson));
+  }
+
+  private static List<Person> generatePerson(int count)  {
+    List<String> namePerson = List.of("Tolstoy", "Bulgakov", "Dostoevskii", "Gogol", "Martin", "Hugo", "Duma");
+    List<String> nameDepartment = List.of("marketing",
+            "logistics" ,
+            "production",
+            "commercial");
+    List<Person> listPeople = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      Person person =new Person(getRandom(namePerson),
+              ThreadLocalRandom.current().nextInt(18, 50),
+              ThreadLocalRandom.current().nextInt(35000, 100000)*1.0,
+              new Department(getRandom(nameDepartment)));
+
+      listPeople.add(person);
+    }
+    return listPeople;
+  }
+
+
+  private static <T> T getRandom(List<? extends T> items) {
+    int randomIndex = ThreadLocalRandom.current().nextInt(0, items.size());
+    return items.get(randomIndex);
+  }
+
+
 
   /**
    * Найти самого молодого сотрудника
    */
   static Optional<Person> findMostYoungestPerson(List<Person> people) {
     // FIXME: ваша реализация здесь
+    Optional<Person> min = people.stream()
+            .min(Comparator.comparingInt(Person::getAge));
+    return min;
+
+
   }
 
   /**
@@ -33,6 +144,11 @@ public class Homework {
    */
   static Optional<Department> findMostExpensiveDepartment(List<Person> people) {
     // FIXME: ваша реализация здесь
+    Person answer =  people.stream()
+                    .max(Comparator.comparing(Person::getSalary)).get();
+
+
+    return Optional.ofNullable(answer.getDepart());
   }
 
   /**
@@ -40,6 +156,9 @@ public class Homework {
    */
   static Map<Department, List<Person>> groupByDepartment(List<Person> people) {
     // FIXME: ваша реализация здесь
+    return people.stream()
+            .collect(Collectors.groupingBy(Person::getDepart));
+
   }
 
   /**
@@ -47,6 +166,10 @@ public class Homework {
    */
   static Map<String, List<Person>> groupByDepartmentName(List<Person> people) {
     // FIXME: ваша реализация здесь
+   return people.stream()
+            .collect(Collectors.groupingBy(o -> o.getDepart().getName()));
+
+
   }
 
   /**
@@ -54,6 +177,17 @@ public class Homework {
    */
   static Map<String, Person> getDepartmentOldestPerson(List<Person> people) {
     // FIXME: ваша реализация здесь
+    return people.stream()
+            .collect(Collectors.toMap(
+                    o -> o.getDepart().getName(),
+                    Function.identity(),
+                    (a,b)-> {
+                      if (a.getAge()>b.getAge()){
+                        return a;
+                      }
+                      return b;
+                    }
+                    ));
   }
 
   /**
@@ -62,6 +196,9 @@ public class Homework {
    */
   static List<Person> cheapPersonsInDepartment(List<Person> people) {
     // FIXME: ваша реализация здесь
+
+
+    return  null;
   }
 
 }
